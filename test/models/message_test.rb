2667bf0b8b9826c6ff89305a82a79e69384b7865
Jake Smith <jakehockey10@gmail.com>
2017-03-28 19:45:05 -0600

    Prep for launch

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -14,4 +14,14 @@ class MessageTest < ActiveSupport::TestCase
     @message.content = " "
     assert !@message.valid?
   end
+
+  test "should notice mentions" do
+    @message.content = "@#{users(:example).username} is a jerk!"
+    assert_not @message.mentions.empty?
+  end
+
+  test "should notice lack of mentions" do
+    @message.content = "Somebody is a jerk, I just don't know their username!"
+    assert @message.mentions.empty?
+  end
 end
