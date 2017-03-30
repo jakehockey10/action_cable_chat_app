f9bfc17d24750fafff1f38a64d1cff2c4e5572a3
Jake Smith <jakehockey10@gmail.com>
2017-03-29 19:47:52 -0600

    wildcarding for now just to get it to work

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module ChatApp
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'localhost:4200', '127.0.0.1:4200'
-        resource 'messages', headers: :any, methods: [:get]
+        resource '*', headers: :any, methods: [:get]
       end
     end
   end
